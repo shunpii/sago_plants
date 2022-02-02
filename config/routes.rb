50f65bbdb3191eb5f8a7cd0810bc3c86f38e86c0
shunpii <shun.s0029@gmail.com>
2022-02-02 10:12:52 +0000

    [add]devise/admin,publicのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :publics
-  devise_for :admins
+  scope module: :public do
+    devise_for :publics, path: "public", controller: {
+      registrations: "publics/registrations",
+      sessions: "publics/sessions"
+    }
+  end
+  
+  scope module: :admin do
+    devise_for :admins, path: "admin", controller: {
+      registrations: "admins/registrations",
+      sessions: "admins/sessions"
+    }
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
