942d9c00cea5c41ce5f5129436c5afe74c5c176c
shunpii <shun.s0029@gmail.com>
2022-02-02 13:16:12 +0000

    [update]public/topのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope module: :public do
-    root to: "homes#top"
+    get "top"
     devise_for :publics, path: "public", controller: {
       registrations: "publics/registrations",
       sessions: "publics/sessions"
