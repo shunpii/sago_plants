5bb209eb53ee7287d52d308bf6eb22ab81b0280c
shunpii <shun.s0029@gmail.com>
2022-02-05 13:59:37 +0000

    [add]ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  devise_for :managers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'  
+  }
+  
+  devise_for :managers, controllers: {
+    sessions:      'managers/sessions',
+    passwords:     'managers/passwords',
+    registrations: 'managers/registrations'  
+  }
+  
 end
