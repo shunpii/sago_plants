2747e390641a694cdf419144dce8bf8e4edf6cb2
shunpii <shun.s0029@gmail.com>
2022-02-07 03:08:19 +0000

    [update]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_134537) do
+ActiveRecord::Schema.define(version: 2022_02_07_030014) do
 
   create_table "managers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_05_134537) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_managers_on_email", unique: true
